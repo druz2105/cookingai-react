aa4293afb909efec5ab2c81586bc4df7acb5d679
dhruvil <druz2105@gmail.com>
2025-01-10 14:06:00 -0500

    build: :rocket: Updated dockerfile for project

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,18 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from 'tailwindcss'
 
 export default defineConfig({
+  base: "/",
   plugins: [react()],
+  preview: {
+    port: 8080,
+    strictPort: true,
+  },
+  server: {
+    port: 8080,
+    strictPort: true,
+    host: true,
+    origin: "http://0.0.0.0:8080",
+  },
   css: {
     postcss: {
       plugins: [tailwindcss()],
